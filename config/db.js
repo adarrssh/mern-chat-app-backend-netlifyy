7eb14ed784d8ef17c66622eb018b91e5ac2a50b8
adarrssh <adarsh00502@gmail.com>
2024-01-23 19:29:04 +0530

    fix: remove console.log

--- config/db.js
+++ config/db.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose')
 const connectDB = async () =>{
 
     try {
-        console.log(`mongo uri ......  ${process.env.MONGO_URI}`)
-        console.log(` ur ......  ${process.env.URL}`)
-
         const conn = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser : true,
             useUnifiedTopology : true        })
