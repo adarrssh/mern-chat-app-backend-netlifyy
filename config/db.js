3413b31233ee0e8490a7605313aba8ae75639c5e
Adarsh <85433137+adarrssh@users.noreply.github.com>
2024-01-23 19:17:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () =>{
 
     try {
+        console.log(`mongo uri ......  ${process.env.MONGO_URI}`)
         const conn = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser : true,
             useUnifiedTopology : true        })
